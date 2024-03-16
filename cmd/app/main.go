3d95c8d3d7bcde6ee261e3a9a528401ca12ed0f7
Evgenia Zavoyskikh <evgeniazavojskih@gmail.com>
2024-03-16 12:44:48 +0300

    add main

---
+++ cmd/app/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"vk-film-library/config"
+	v1 "vk-film-library/internal/controller/http/v1"
+	"vk-film-library/internal/httpserver"
+	"vk-film-library/internal/repo"
+	"vk-film-library/internal/service"
+	"vk-film-library/pkg/logger"
+	"vk-film-library/pkg/postgres"
+)
+
+func main() {
+	log := logger.GetLogger()
+	cfg := config.GetConfig(log)
+
+	log.Info("initializing postgres")
+	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		cfg.Postgres.Username, cfg.Postgres.Password, cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Database)
+	client, err := postgres.NewClient(context.Background(), 3, connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	log.Info("initializing repositories")
+	repos := repo.NewRepositories(client)
+
+	log.Info("initializing services")
+	deps := service.ServicesDependencies{
+		Repos:    repos,
+		SignKey:  cfg.JWT.SignKey,
+		TokenTTL: cfg.JWT.TokenTTL,
+	}
+	services := service.NewServices(deps)
+
+	mux := http.NewServeMux()
+	v1.NewRouter(mux, services, log)
+	log.Info("starting http server")
+	log.Debug("server port: %s", cfg.HTTPServer.Port)
+	address := fmt.Sprintf("%s:%s", cfg.HTTPServer.Host, cfg.HTTPServer.Port)
+	httpServer := httpserver.New(mux, address)
+	go func() {
+		err = httpServer.Start()
+		if err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	err = httpServer.Stop()
+	if err != nil {
+		panic(errors.Wrap(err, "Httpserver shutdown"))
+	}
+	log.Debug("Httpserver exited")
+}
---
+++ go.mod
@@ -0,0 +1,44 @@
+module vk-film-library
+
+go 1.20
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang/mock v1.6.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/pashagolub/pgxmock/v2 v2.12.0
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.8.1
+	github.com/swaggo/http-swagger/v2 v2.0.2
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/mod v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
